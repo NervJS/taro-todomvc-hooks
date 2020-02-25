b3716a2e077a4f1c654d0eaffaa190df7a884582
yuche <i@yuche.me>
2020-02-25 15:40:53 +0800

    更改依赖

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  presets: [['@babel/env', {
-    // modules: 'false'
-  }], [
-    '@babel/preset-react', {
-      'pragma': 'React.createElement' // default pragma is React.createElement
-    }
-  ]],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  presets: [
+    ['taro', {
+      framework: 'react',
+      ts: false
+    }]
+  ]
 }
